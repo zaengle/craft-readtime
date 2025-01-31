d2c2fc0aec8ea5f86690465ded8a187f500af455
Jenny Medeiros <jennymedeiros@Mac.medeiroshome>
2025-01-31 18:43:43 -0500

    Add __toString to model

--- src/models/ReadTime.php
+++ src/models/ReadTime.php
@@ -35,4 +35,8 @@ class ReadTime extends Model
     {
         return $this->seconds ? DateTimeHelper::humanDuration($this->seconds, false) : '';
     }
+    public function __toString(): string
+    {
+        return $this->human();
+    }
 }
